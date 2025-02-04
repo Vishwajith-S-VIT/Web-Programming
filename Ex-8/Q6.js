ff52516a53fde2616939a52c8ef412b134dfcb27
Vishwajith-S-VIT <vishwajith.s2023@vitstudent.ac.in>
2025-02-04 20:50:51 +0530

    Added Ex-8

---
+++ Ex-8/Q6.js
@@ -0,0 +1,5 @@
+function Total(){
+    n1 = parseInt(document.getElementById("t1").value);
+    n2 = parseInt(document.getElementById("t2").value);
+    document.getElementById("t3").value = n1*10 + n2*5;
+}
